8df5022c0b6e4c47e2a3c5fdb79308e26ed99ed5
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-03-15 01:14:34 +0530

    updated theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+"use client"
 import { Button } from "@/components/ui/button";
+import { useGlobalModal } from "@/hooks/GlobalModal";
 import Image from "next/image";
 
 export default function Home() {
+  const globalModal = useGlobalModal();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Click me</Button>
+      <Button
+        onClick={() => {
+          globalModal.title = "Hello"
+          globalModal.description = "This is a description"
+          globalModal.children = <div className="flex flex-col items-center justify-center">
+
+            <p className="text-center">This is a modal</p>
+          </div>
+          globalModal.onOpen()
+        }}
+      >Click me</Button>
     </main>
   );
 }
