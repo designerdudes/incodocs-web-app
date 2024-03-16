b461d3f0804b4c22c8af06f317840efbd595d52a
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-03-17 00:08:50 +0530

    updated code

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,23 +1,48 @@
-import React from 'react'
-import { Button } from './ui/button'
+"use client"
+import React, { useState } from 'react';
+import { Button } from './ui/button';
+import { Plus } from 'lucide-react';
 
 function Sidebar() {
+    const [hoveredItem, setHoveredItem] = useState(null);
+
+    const handleMouseEnter = (index: any) => {
+        setHoveredItem(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredItem(null);
+    };
+
+    const menuItems = [
+        "Home",
+        "About",
+        "Services",
+        "Contact"
+    ];
+
     return (
         <div className='w-64 bg-primary h-screen text-white'>
             <div className='flex items-center justify-center h-16 bg-primary-dark'>
                 <h1 className='text-2xl font-bold'>Sidebar</h1>
             </div>
             <div className='flex flex-col gap-4'>
-
-                <div className='text-secondary-foreground text-white hover:bg-[#6d6ce3] py-1 px-4'>Home</div>
-                <div className='text-secondary-foreground text-white hover:bg-[#6d6ce3] py-1 px-4'>About</div>
-                <div className='text-secondary-foreground text-white hover:bg-[#6d6ce3] py-1 px-4'>Services</div>
-                <div className='text-secondary-foreground text-white hover:bg-[#6d6ce3] py-1 px-4'>Contact</div>
-
+                {menuItems.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`text-secondary-foreground text-white hover:bg-[#6d6ce3] py-1 px-4 flex justify-between ${hoveredItem === index ? 'hovered' : ''}`}
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <span>{item}</span>
+                        {hoveredItem === index &&
+                            <Plus className='w-6 h-6 p-1 rounded-lg cursor-pointer hover:bg-primary' style={{ opacity: hoveredItem === index ? 1 : 0, transition: 'opacity 0.3s ease-in-out' }} />
+                        }
+                    </div>
+                ))}
             </div>
-
         </div>
-    )
+    );
 }
 
-export default Sidebar
+export default Sidebar;
