cfc575b2ea2f553de5d9a35e7b557dfbde187c14
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-03-17 01:55:21 +0530

    added home page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,23 +1,15 @@
 "use client"
 import { Button } from "@/components/ui/button";
-import { useGlobalModal } from "@/hooks/GlobalModal";
-import Image from "next/image";
+import Heading from "@/components/ui/heading";
+
 
 export default function Home() {
-  const globalModal = useGlobalModal();
-  return (
-    <main className="flex h-full flex-col items-center justify-between p-24">
-      <Button
-        onClick={() => {
-          globalModal.title = "Hello"
-          globalModal.description = "This is a description"
-          globalModal.children = <div className="flex flex-col items-center justify-center">
 
-            <p className="text-center">This is a modal</p>
-          </div>
-          globalModal.onOpen()
-        }}
-      >Click me</Button>
+  return (
+    <main className="flex h-full flex-col p-20">
+      <div>
+        <Heading className="text-4xl" title="Welcome, Arshad!" />
+      </div>
     </main>
   );
 }
