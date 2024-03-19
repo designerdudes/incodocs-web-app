3d96e6541c60a425eba4392597bf67f0945d6e0e
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-03-19 06:41:32 +0530

    updated code

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,53 @@
+import { Metadata } from "next"
+import Image from "next/image"
+import Link from "next/link"
+
+import { BrandName } from "@/lib/constants"
+import { LoginForm } from "@/components/forms/loginForm"
+import { RegisterForm } from "@/components/forms/RegisterForm"
+
+export const metadata: Metadata = {
+    title: 'Create an Account | Incodocs',
+    description: 'Create an Account | Incodocs',
+}
+
+export default function AuthenticationPage() {
+
+    return (
+        <>
+
+            <div className="container relative hidden h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+
+                <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:text-black dark:border-r lg:flex">
+                    <div className="absolute inset-0 bg-primary" />
+                    <div className="relative z-20 flex items-center text-lg font-bold">
+                        <Image
+                            src={"/assets/logos/SymbolWhite.svg"}
+                            width={35}
+                            height={35}
+                            alt="Logo"
+                            className="mr-2 dark:invert"
+                        />
+                        {BrandName}
+                    </div>
+                    <div className="relative z-20 mt-auto">
+                        <blockquote className="space-y-2">
+                            <p className="text-lg">
+                                &ldquo;A Great way to Manage Your <b>ExIm Business.</b>&rdquo;
+                            </p>
+                            <footer className="text-sm">Created By Team <Link className="underline-offset-4 hover:underline" href="https://designerdudes.in">DesignerDudes</Link></footer>
+                        </blockquote>
+                    </div>
+                </div>
+                <div className="lg:p-8">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+
+                        <RegisterForm />
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
---
+++ components/forms/RegisterForm.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import { Icons } from "@/components/ui/icons"
+import { Button } from "../ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "../ui/card"
+import { Input } from "../ui/input"
+import { Label } from "../ui/label"
+import { ArrowRight } from "lucide-react"
+import axios from "axios"
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+import { BrandName } from "@/lib/constants"
+
+export function RegisterForm() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [message, setMessage] = useState('')
+
+    const router = useRouter()
+
+    const user = {
+        email: email,
+        password: password,
+    }
+    const onSubmit = async () => {
+        setLoading(true)
+        if (email === "") {
+            setError(true)
+            setMessage("Email is required")
+            setLoading(false)
+        }
+        if (password === "") {
+            setError(true)
+            setMessage("Password is required")
+            setLoading(false)
+        }
+        if (email === "" || password === "") {
+            setError(true)
+            setMessage("Email and Password is required")
+            setLoading(false)
+        }
+        else {
+            try {
+
+                setLoading(true)
+                const res = await axios.post('/api/auth/login', user)
+                console.log(" login successfull")
+                console.log(res.data)
+
+                router.refresh()
+            } catch (error: any) {
+                setLoading(false)
+                setError(true)
+                setMessage(error.response.data.error)
+                console.error(error.response.data.error)
+
+
+            } finally {
+                setLoading(false)
+
+            }
+        }
+
+    }
+
+
+
+    return (
+        <Card className=" shadow-sm shadow-[#00000042]  dark:shadow-[#ffffff42]">
+            <CardHeader className="space-y-1">
+                <CardTitle className="text-2xl font-bold leading-tight tracking-tighter">Create an Account at {BrandName}</CardTitle>
+
+            </CardHeader>
+            <CardContent className="grid gap-4">
+
+                <div className="grid gap-2">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" disabled={loading} value={email} onChange={(e: any) => setEmail(e.target.value)} placeholder="mohammed@incodocs.net" />
+
+                </div>
+                <div className="grid gap-2">
+                    <Label htmlFor="password">Password</Label>
+                    <Input disabled={loading} id="password" value={password} onChange={(e: any) => setPassword(e.target.value)} type="password" placeholder="******" />
+                    {error && <p className="text-red-500 text-sm">{message}</p>}
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Button disabled={loading} onClick={onSubmit} className="w-full hover:gap-1 transition-all" variant="default">
+                    Continue via Email
+                    {
+                        loading ? <Icons.spinner className="ml-2 w-4 animate-spin" /> : <ArrowRight className="ml-2 w-4" />
+                    }
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
