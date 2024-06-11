7deec8101c01a1ade2864df808bdb826b43b85c6
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-06-12 01:34:49 +0530

    updated code

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -181,7 +181,8 @@ export function DataTable<TData, TValue>({
                                     data-state={row.getIsSelected() && "selected"}
                                 >
                                     {row.getVisibleCells().map((cell) => (
-                                        <TableCell key={cell.id}>
+                                        <TableCell className="h-fit py-2 text-sm" key={cell.id}
+                                        >
                                             {flexRender(cell.column.columnDef.cell, cell.getContext())}
                                         </TableCell>
                                     ))}
