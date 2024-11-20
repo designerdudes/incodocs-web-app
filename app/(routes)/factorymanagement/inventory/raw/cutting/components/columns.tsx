4dd4442b6e92dea3365e3b32df74c0e657a22129
Mohammed Arshad <mohammedarshad.arsh@gmail.com>
2024-11-20 14:39:41 +0530

    updated code

---
+++ app/(routes)/factorymanagement/inventory/raw/cutting/components/columns.tsx
@@ -0,0 +1,165 @@
+"use client"
+import { Button } from "@/components/ui/button"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown } from "lucide-react"
+import { Checkbox } from "@/components/ui/checkbox"
+import CellAction from "./cell-actions"
+import moment from "moment"
+
+export type Block = {
+    _id: string
+    blockNumber: string
+    blockLotName: string
+    materialType: string
+    numberofSlabs: string
+    isActive: boolean
+    createdAt: string
+    updatedAt: string
+    weight: string
+    height: string
+    breadth: string
+    length: string
+    volume: string
+    status: string
+}
+
+export const columns: ColumnDef<Block>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value: any) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "blockLotName",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Block&apos;s Lot Name
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <div className="capitalize">
+                {row.original.blockLotName}
+            </div>
+        ),
+    },
+    {
+        accessorKey: "blockNumber",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Block Number
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <div className="capitalize">
+                {row.original.blockNumber}
+            </div>
+        ),
+    },
+    {
+        accessorKey: "materialType",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Material Type
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <div className="capitalize">
+                {row.original.materialType}
+            </div>
+        ),
+    },
+    {
+        accessorKey: "numberofSlabs",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Total Slabs
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <div className="capitalize">
+                {row.original.numberofSlabs}
+            </div>
+        ),
+    },
+    {
+        accessorKey: "status",
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+                onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+            >
+                Blocks Status
+                <ArrowUpDown className="ml-2 h-4 w-4" />
+            </Button>
+        ),
+        cell: ({ row }) => (
+            <div className="capitalize">
+                {row.original.status}
+            </div>
+        ),
+    },
+    // {
+    //     accessorKey: "createdAt",
+    //     header: ({ column }) => (
+    //         <Button
+    //             variant="ghost"
+    //             onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    //         >
+    //             Block Created Date
+    //             <ArrowUpDown className="ml-2 h-4 w-4" />
+    //         </Button>
+    //     ),
+    //     cell: ({ row }) => (
+    //         <div>
+    //             {moment(row.original.createdAt).format("DD MMM YYYY")}
+    //         </div>
+    //     ),
+    // },
+
+    {
+
+        header: ({ column }) => (
+            <Button
+                variant="ghost"
+            >
+                Action
+            </Button>
+        ),
+
+        id: "actions",
+        cell: ({ row }) => <CellAction data={row.original} />
+    },
+]
---
+++ app/(routes)/factorymanagement/inventory/raw/cutting/page.tsx
@@ -0,0 +1,68 @@
+import { DataTable } from '@/components/ui/data-table';
+import { Separator } from '@/components/ui/separator';
+import React from 'react';
+import CreateNewLotButton from '../lots/components/CreateNewLotButton';
+import Heading from '@/components/ui/heading';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
+import { ChevronLeft } from 'lucide-react';
+import { columns } from './components/columns';
+import { Block } from './components/columns';
+
+const CuttingInventoryPage: React.FC = () => {
+
+    const data: Block[] = [
+        {
+            _id: "65f8fb0fc4417ea5a14fbd82",
+            materialType: "Granite",
+            numberofSlabs: "",
+            isActive: true,
+            createdAt: "2024-03-19T02:40:15.954Z",
+            height: "54",
+            breadth: "3.2",
+            length: "4.2",
+            blockNumber: '12345',
+            blockLotName: 'LOT 1',
+            updatedAt: '',
+            weight: '45',
+            volume: '785',
+            status: 'in Cutting'
+        },
+    ];
+
+    return (
+        <div className="w-auto space-y-2 h-full flex p-6 flex-col">
+            <div className="topbar w-full flex justify-between items-center">
+                <Link href="../raw">
+                    <Button variant="outline" size="icon" className="w-8 h-8 mr-4">
+                        <ChevronLeft className="h-4 w-4" />
+                        <span className="sr-only">Back</span>
+                    </Button>
+                </Link>
+                <div className="flex-1">
+                    <Heading className="leading-tight" title="Slabs In Cutting Inventory" />
+                    <p className="text-muted-foreground text-sm mt-2">
+                        Efficiently track and manage slabs in the cutting inventory with detailed insights into their dimensions, weight, and progress through the cutting process.
+                    </p>
+                </div>
+                {/* Move the interactivity to the client-side button component */}
+                {/* <CreateNewLotButton /> */}
+            </div>
+            <Separator orientation="horizontal" />
+            <div className="w-250 container mx-auto py-10">
+                <DataTable
+                    bulkDeleteIdName="_id"
+                    bulkDeleteTitle="Are you sure you want to delete the selected Lots?"
+                    bulkDeleteDescription="This will delete all the selected Lots, and they will not be recoverable."
+                    bulkDeleteToastMessage="Selected lots deleted successfully"
+                    deleteRoute="/category/ids"
+                    searchKey="name"
+                    columns={columns}
+                    data={data as any}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CuttingInventoryPage;
