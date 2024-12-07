a7dcbc2ed00f565977bc31cf742a37846afbb7e2
Farmohammed07 <farooqmohd5744@gmail.com>
2024-12-07 19:41:09 +0530

    shipping page created

---
+++ app/(routes)/documentation/shipment/dashboard/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+import TopBar from '@/components/topbar'
+import { Sidebar } from '@/components/ui/sidebar'
+
+export const metadata: Metadata = {
+    title: 'Admin Dashboard| APLus Laundry',
+    description: 'Admin dashboard for APlus Laundry',
+}
+
+interface DashboardLayoutProps {
+    children: React.ReactNode
+}
+
+
+export default function DashboardLayout({
+    children
+}: DashboardLayoutProps) {
+    return (
+        <>
+            <div className='flex flex-row h-screen'>
+
+
+                <Sidebar />
+                <div className='flex w-full flex-col'>
+                    <TopBar />
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
