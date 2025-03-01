9a94987cbf88ad1f710c5258b8eae965868f3af8
Shaikh Adnan <shaikhadnan0127@gmail.com>
2025-03-01 18:04:44 +0530

    Parties page , Data Integration pending

---
+++ app/(routes)/documentation/parties/components/consigneeColumn.tsx
@@ -0,0 +1,134 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
+import ConsigneeCellActions from "./consigneeCellAction";
+
+
+export type Consignee = {
+  _id: string;
+  name: string;
+  address: string;
+  responsiblePerson: string;
+  telephoneNo: number;
+  email: string;
+  organizationId: string;
+};
+
+export const consigneecolumns: ColumnDef<Consignee>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value: any) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value: any) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Consignee Name
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original?.name}</div>
+    ),
+    filterFn: "includesString",
+  },
+  {
+    accessorKey: "address",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Address
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original?.address}</div>
+    ),
+    filterFn: "includesString",
+    // ensures it filters by includes method (you can define custom filter functions)
+  },
+
+  {
+    accessorKey: "responsiblePerson",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Responsible Person
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original?.responsiblePerson}</div>
+    ),
+  },
+  {
+    accessorKey: "mobileNo",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Mobile No
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => (
+      <div className="capitalize">{row.original?.telephoneNo}</div>
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <Button
+        variant="ghost"
+        onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      >
+        Email
+        <ArrowUpDown className="ml-2 h-4 w-4" />
+      </Button>
+    ),
+    cell: ({ row }) => <div className="capitalize">{row.original?.email}</div>,
+  },
+  {
+          header: ({ column }) => (
+              <Button
+                  variant="ghost"
+              >
+                  Action
+              </Button>
+          ),
+  
+          id: "actions",
+          cell: ({ row }) => <ConsigneeCellActions data={row.original} />
+      },
+  
+];
