ed33f97d59c262e612d3c2d239599edeed44dd1e
shija uddin <khajashija@gmail.com>
2025-04-22 17:06:42 +0530

    updated the routing inventory dashboard

--- app/(routes)/[organizationId]/[factoryid]/factorymanagement/inventory/page.tsx
+++ app/(routes)/[organizationId]/[factoryid]/factorymanagement/inventory/page.tsx
@@ -4,7 +4,7 @@ import Heading from '@/components/ui/heading'
 import { ChevronLeft } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
-import { InventoryCards } from '@/lib/constants';
+import { InventoryCards } from '@/lib/constants'
 
 interface Props {
   params: {
@@ -15,7 +15,7 @@ interface Props {
 function Page({ params }: Props) {
 
   return (
-    <div className="flex  flex-col p-6">
+    <div className="flex flex-col p-6">
       <div className="flex justify-between items-center gap-2">
         <Link href="./">
           <Button variant="outline" size="icon" className="w-8 h-8 mr-4">
@@ -24,27 +24,35 @@ function Page({ params }: Props) {
           </Button>
         </Link>
         <div className="flex-1">
-          <Heading className='leading-tight ' title='Inventory' />
-          <p className='mt-2'>Effectively oversee your factory&apos;s Inventory and proccessing goods inventory.</p>
+          <Heading className="leading-tight" title="Inventory" />
+          <p className="mt-2">
+            Effectively oversee your factory&apos;s Inventory and processing goods inventory.
+          </p>
         </div>
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
         {
-          InventoryCards?.map((card, index) => (
-            <StatsCard
-              key={index}
-              title={card.title}
-              stat={card.value}
-              icon={card.icon}
-              desc=""
-              href={card.buttonUrl}
-              factoryId={params.factoryid} />
-          ))
+          InventoryCards?.map((card, index) => {
+            // Ensure that the href is prefixed with the factoryid
+            const updatedButtonUrl = `/${params.factoryid}${card.buttonUrl}`;
+
+            return (
+              <StatsCard
+                key={index}
+                title={card.title}
+                stat={card.value}
+                icon={card.icon}
+                desc=""
+                href={updatedButtonUrl} // Corrected href with factoryid
+                factoryId={params.factoryid}
+              />
+            );
+          })
         }
       </div>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
