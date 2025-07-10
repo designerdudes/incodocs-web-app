6cbfc17ccfcb247c651037a2c1a2688050df58df
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-07-10 12:45:04 +0530

    parties supplier create new

--- app/(routes)/[organizationId]/[factoryid]/factorymanagement/parties/supplier/createNew/page.tsx
+++ app/(routes)/[organizationId]/[factoryid]/factorymanagement/parties/supplier/createNew/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import SupplierFormPage from "@/components/forms/SupplierFormPage";
 
-function page() {
-  return (
-    <div>
-      create
-    </div>
-  )
+interface Props {
+  params: {
+    factoryid: string;
+    organizationId: string;
+  };
 }
 
-export default page
+export default function CreateNewSupplierPage({ params }: Props) {
+  return (
+    <div className="w-full space-y-2 h-full flex p-6 flex-col">
+      <div className="topbar w-full flex items-center justify-between">
+        <Link href="../">
+          <Button variant="outline" size="icon" className="w-8 h-8 mr-4">
+            <ChevronLeft className="h-4 w-4" />
+            <span className="sr-only">Back</span>
+          </Button>
+        </Link>
+        <div className="flex-1">
+          <Heading className="leading-tight" title="Add Supplier" />
+          <p className="text-muted-foreground text-sm">
+            Complete the form below to add a new Supplier.
+          </p>
+        </div>
+      </div>
+      <Separator orientation="horizontal" />
+      <div className="container mx-auto">
+        <SupplierFormPage params={params} />
+      </div>
+    </div>
+  );
+}
